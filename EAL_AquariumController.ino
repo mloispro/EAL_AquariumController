062afcbd4674cff88483121270d4bcc2a906cfbe
mloispro <olson_mitchell@hotmail.com>
2016-04-27 19:20:59 -0500

    nr working good

--- EAL_AquariumController.ino
+++ EAL_AquariumController.ino
@@ -82,7 +82,7 @@ void setup() {
 	RTCExt::Init();
 	//_lcdMenuController = LCDMenuController();
 	
-	TestLoadNRMem();
+	TestNRMem();
 
 	return;
 
@@ -99,10 +99,10 @@ void setup() {
 	//	_lcdMenuController.SelectMainMenu();
 
 }
-static NextRunMemory _doseInfo;
-static NextRunMemory _feedInfo;
+//static NextRunMemory _doseInfo;
+//static NextRunMemory _feedInfo;
 void TestLoadNRMem(){
-	SerialExt::Debug("--Load_FeedInfo--");
+	/*SerialExt::Debug("--Load_FeedInfo--");
 	int accType = static_cast<int>(AccessoryType::Feeder);
 	_feedInfo.AccType = accType;
 	_feedInfo = RTCExt::GetNextRunMem(_feedInfo);
@@ -110,47 +110,83 @@ void TestLoadNRMem(){
 	SerialExt::Debug("--Load_DoseInfo--");
 	int accType2 = static_cast<int>(AccessoryType::DryDoser);
 	_doseInfo.AccType = accType2;
-	_doseInfo = RTCExt::GetNextRunMem(_doseInfo);
+	_doseInfo = RTCExt::GetNextRunMem(_doseInfo);*/
 }
 void TestNRMem(){
 
-	SerialExt::Debug("--FeedInfo--");
-	
-	int accType = static_cast<int>(AccessoryType::Feeder);
-	
-	_feedInfo.AccType = accType;
-	_feedInfo.CountDown = 1000L;
-	_feedInfo.LastRun = 2000L;
-	_feedInfo.NextRun = 3000L;
-	_feedInfo.RunEvery = 4000L;
-	_feedInfo.ShakesOrTurns = (short)5;
-	
-	_feedInfo = RTCExt::SaveNextRunMem(_feedInfo);
-	_feedInfo = RTCExt::GetNextRunMem(_feedInfo);
+	auto feederAcc = AccessoryType::Feeder;
+	auto doserAcc = AccessoryType::DryDoser;
+	RTCExt::SetRunEvery(2, feederAcc);
+	RTCExt::SetRunEvery(3, doserAcc);
 
-	SerialExt::Debug("--DoseInfo--");
+	RTCExt::UpdateNextRun(feederAcc);
+	RTCExt::UpdateNextRun(doserAcc);
 
-	int accType2 = static_cast<int>(AccessoryType::DryDoser);
+	delay(1000);
 
-	_doseInfo.AccType = accType2;
-	_doseInfo.CountDown = 6000L;
-	_doseInfo.LastRun = 7000L;
-	_doseInfo.NextRun = 8000L;
-	_doseInfo.RunEvery = 9000L;
-	_doseInfo.ShakesOrTurns = (short)8;
+	RTCExt::UpdateNextRun(feederAcc);
+	RTCExt::UpdateNextRun(doserAcc);
 
-	_doseInfo = RTCExt::SaveNextRunMem(_doseInfo);
-	_doseInfo = RTCExt::GetNextRunMem(_doseInfo);
+	delay(10000);
 
-	SerialExt::Debug("--FeedInfo Update--");
-	_feedInfo.CountDown = 1500L;
-	_feedInfo = RTCExt::SaveNextRunMem(_feedInfo);
-	_feedInfo = RTCExt::GetNextRunMem(_feedInfo);
+	RTCExt::UpdateNextRun(feederAcc);
+	RTCExt::UpdateNextRun(doserAcc);
 
-	SerialExt::Debug("--DoseInfo Update--");
-	_doseInfo.CountDown = 6500L;
-	_doseInfo = RTCExt::SaveNextRunMem(_doseInfo);
-	_doseInfo = RTCExt::GetNextRunMem(_doseInfo);
+	delay(60000);
+
+	RTCExt::UpdateNextRun(feederAcc);
+	RTCExt::UpdateNextRun(doserAcc);
+
+	RTCExt::SetRunEvery(5, feederAcc);
+	RTCExt::SetRunEvery(6, doserAcc);
+
+	delay(20000);
+
+	RTCExt::UpdateNextRun(feederAcc);
+	RTCExt::UpdateNextRun(doserAcc);
+
+	delay(140000);
+
+	RTCExt::UpdateNextRun(feederAcc);
+	RTCExt::UpdateNextRun(doserAcc);
+
+	//SerialExt::Debug("--FeedInfo--");
+	//
+	//int accType = static_cast<int>(AccessoryType::Feeder);
+	//
+	//_feedInfo.AccType = accType;
+	//_feedInfo.CountDown = 0L;
+	//_feedInfo.LastRun = 0L;
+	//_feedInfo.NextRun = 0L;
+	//_feedInfo.RunEvery = 0L;
+	//_feedInfo.ShakesOrTurns = (short)0;
+	//
+	//_feedInfo = RTCExt::SaveNextRunMem(_feedInfo);
+	//_feedInfo = RTCExt::GetNextRunMem(_feedInfo);
+
+	//SerialExt::Debug("--DoseInfo--");
+
+	//int accType2 = static_cast<int>(AccessoryType::DryDoser);
+
+	//_doseInfo.AccType = accType2;
+	//_doseInfo.CountDown = 0L;
+	//_doseInfo.LastRun = 0L;
+	//_doseInfo.NextRun = 0L;
+	//_doseInfo.RunEvery = 0L;
+	//_doseInfo.ShakesOrTurns = (short)0;
+
+	//_doseInfo = RTCExt::SaveNextRunMem(_doseInfo);
+	//_doseInfo = RTCExt::GetNextRunMem(_doseInfo);
+
+	////SerialExt::Debug("--FeedInfo Update--");
+	////_feedInfo.CountDown = 1500L;
+	////_feedInfo = RTCExt::SaveNextRunMem(_feedInfo);
+	////_feedInfo = RTCExt::GetNextRunMem(_feedInfo);
+
+	////SerialExt::Debug("--DoseInfo Update--");
+	////_doseInfo.CountDown = 6500L;
+	////_doseInfo = RTCExt::SaveNextRunMem(_doseInfo);
+	////_doseInfo = RTCExt::GetNextRunMem(_doseInfo);
 
 }
 
